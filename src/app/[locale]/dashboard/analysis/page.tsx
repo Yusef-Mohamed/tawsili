fdf873e074fe685e90246da7b31163b6e6ce2316
Yusef-Mohamed <yoousefmohaamd@gmail.com>
2024-03-14 19:06:02 +0200

    0

--- src/app/[locale]/dashboard/analysis/page.tsx
+++ src/app/[locale]/dashboard/analysis/page.tsx
@@ -42,17 +42,17 @@ const Page = ({ params }: { params: { locale: string } }) => {
   unstable_setRequestLocale(params.locale);
   const text = useTranslations("analysis");
   return (
-    <main className="px-6">
-      <h2 className="text-2xl font-semibold">{text("title")}</h2>
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
+    <main>
+      <h2 className="px-6 text-2xl font-semibold">{text("title")}</h2>
+      <div className="grid px-6 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
         {cards.map((card, index) => (
           <AnalysisCard key={index} card={card} />
         ))}
       </div>
-      <h2 className="mt-4 text-2xl font-semibold">
+      <h2 className="mt-4 px-6 text-2xl font-semibold">
         {text("weakly_net_profit")}
       </h2>
-      <div className="w-full p-8 border rounded-xl mt-8">
+      <div className="overflow-hidden mx-auto w-[85vw] sm:w-auto  sm:mx-6 p-4 sm:p-8 border rounded-xl mt-8">
         <div className="flex items-center gap-4">
           <BsChevronRight className="text-4xl text-gray-400 arOnly" />
           <BsChevronLeft className="text-4xl text-gray-400 enOnly" />
